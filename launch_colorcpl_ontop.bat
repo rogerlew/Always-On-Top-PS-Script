8d0d99dc35cc749b8298a58892be687dd87f7463
Roger Lew <rogerlew@gmail.com>
2023-11-07 21:57:51 -0800

    Create launch_colorcpl_ontop.bat

---
+++ launch_colorcpl_ontop.bat
@@ -0,0 +1,3 @@
+start colorcpl
+SET "APP_NAME=Color Management"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Users\roger\Downloads\Always_On_Top.ps1' -chosenApplication '%APP_NAME%'"
